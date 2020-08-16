6040c7483d23d0f83358e728020c484e8322d32f
Binh Le <lebinh.it@gmail.com>
2020-08-16 12:06:27 +0000

    Use SystemRandom as the secure random generator

--- horse.py
+++ horse.py
@@ -1,8 +1,11 @@
 import random
 import sys
 
+
 with open('Viet11K.txt') as f:
     words = f.read().splitlines()
 dwords = [w for w in words if len(w.split()) == 2 and len(w) < 11]
 count = int(sys.argv[1]) if len(sys.argv) > 1 else 4
-print('.'.join(random.choice(dwords) for _ in range(count)))
+
+rand = random.SystemRandom()
+print('.'.join(rand.choice(dwords) for _ in range(count)))
